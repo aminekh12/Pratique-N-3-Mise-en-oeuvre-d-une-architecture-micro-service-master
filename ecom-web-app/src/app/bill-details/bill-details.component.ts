527ba491216769f4fb52141f2c2047374f58f16f
amine khatmi <a.khatmi@upm.ac.ma7>
2023-02-13 00:01:57 +0100

    un

---
+++ ecom-web-app/src/app/bill-details/bill-details.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-bill-details',
+  templateUrl: './bill-details.component.html',
+  styleUrls: ['./bill-details.component.css']
+})
+export class BillDetailsComponent {
+  billDetails : any;
+  billId!:number;
+  constructor(private http:HttpClient, private router:Router,private route:ActivatedRoute) {
+    this.billId=route.snapshot.params['billId'];
+  }
+  ngOnInit(): void {
+    this.http.get("http://localhost:9999/BILLING-SERVICE/fullBill/"+this.billId).subscribe({
+      next : (data)=>{
+        this.billDetails=data;
+      },
+      error : (err)=>{}
+    });
+  }
+}
---
+++ ecom-web-app/src/app/products/products.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit{
+  products : any;
+  constructor(private http:HttpClient) {
+  }
+  ngOnInit(): void {
+    this.http.get("http://localhost:9999/INVENTORY-SERVICE/products").subscribe({
+      next : (data)=>{
+        this.products=data;
+      },
+      error : (err)=>{}
+    });
+  }
+
+}
